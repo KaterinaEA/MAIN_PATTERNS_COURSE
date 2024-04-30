2214837a4335b70e74158c8720104d9aea4626f6
Южакова Екатерина Андреевна <e.yuzhakova@absolutins.ru>
2024-04-30 18:32:14 +0300

    n-мерный массив

--- src/test/java/lsp_isp_test/MoveTest.java
+++ src/test/java/lsp_isp_test/MoveTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
+
 import static junit.framework.TestCase.assertEquals;
 import static org.mockito.Mockito.*;
 
