9c0d748c24126d24f3f9c3570c817ab8b90a7e32
Южакова Екатерина Андреевна <e.yuzhakova@absolutins.ru>
2024-06-01 12:00:32 +0300

    IoC 01/06/2024

--- src/test/java/iocTest/IocTest.java
+++ src/test/java/iocTest/IocTest.java
@@ -1,13 +1,36 @@
 package iocTest;
 
+import iocModule2L9.CommandMove;
+import iocModule2L9.Init;
+import iocModule2L9.IoC;
 import org.junit.Test;
+
 
 public class IocTest {
 
     @Test
-    public void testInitRegister(){
+    public void testIoCResolveStrategy(){
 
+        Init init = new Init();
 
+        init.init();
+
+        CommandMove move = IoC.resolve("CommandMove");
+
+        move.execute();
+
+    }
+
+    @Test(expected = Exception.class)
+    public void testIoCResolveStrategyException() {
+
+        Init init = new Init();
+
+        init.init();
+
+        CommandMove move = IoC.resolve("CommandMove1");
+
+        move.execute();
 
     }
 
